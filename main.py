96160ec349d87405fc868aa85d03d348fed88709
coyote sady <guiroplayer62@gmail.com>
2026-09-19 00:46:32 -0300

    update main.py

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ print("2 - excluir tarefas")
 print("3 - ver todas tarefas")
 input("qual opcao voce deseja?")
 
+
 def database():
-  with open("tasks.json", "r", encoding="utf-8") as arquivo:
-    banco = json.load(arquivo)
+    with open("task.json", "r", encoding="utf-8") as arquivo:
+        banco = json.load(arquivo)
+    return banco
+
+banco = database()
 
 print(banco)
